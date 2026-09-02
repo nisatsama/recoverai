3cf997c527e7cd2df2b765f1ed2f35230a769619
Nisat Sama <nisatsama7547@gmail.com>
2026-09-02 13:10:40 +0530

    Adding frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Transactions from "./pages/Transactions";
 //import TransactionDetails from "./pages/TransactionDetails";
-import AIDecision from "./pages/AiDecisions";
+import AiDecision from "./pages/AiDecision";
 import AIDecisionDetails from "./pages/AiDecisionDetails";
 function App() {
   return (
@@ -14,7 +14,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/transactions" element={<Transactions />} />
       {/* <Route path="/transactions/:id" element={<TransactionsDetails />} /> */}
-      <Route path="/ai-decisions" element={<AIDecision />} />
+      <Route path="/ai-decisions" element={<AiDecision />} />
       <Route path="/ai-decisions/:id" element={<AIDecisionDetails />} />
     </Routes>
   );
